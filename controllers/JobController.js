3bf57081a5689859e798af2f2dbbff2d4c9c09cd
christonrinaldy <christonrinaldy.geodesy@gmail.com>
2022-07-13 22:26:06 +0700

    pagination api

--- controllers/JobController.js
+++ controllers/JobController.js
@@ -4,12 +4,19 @@ class JobController {
     static getJobs(req, res) {
         let filters = req.query
 
+        const limit = 9
+        const { page } = req.query
+
         axios.get('http://dev3.dansmultipro.co.id/api/recruitment/positions.json')
             .then((raw) => {
                 const resultRaw = Object.assign([], raw.data);
-                var filtered = resultRaw.filter((val) => {
+                var filtered = resultRaw.filter((val, index) => {
+                    if (page != undefined) {
+                        if (index < (page - 1) * limit || index > (page * limit) - 1) return false
+                    }
                     let isTrue = true
                     for (const key in filters) {
+                        if (key == "page") continue;
                         if (key == "full_time" && filters[key] == "true") { isTrue = isTrue && val["type"].toLowerCase() == "full time" }
                         else {
                             isTrue = isTrue && val[key].toLowerCase().includes(filters[key].toLowerCase())
@@ -28,7 +35,6 @@ class JobController {
     static getJobById(req, res) {
         const { id } = req.params;
         const url = `http://dev3.dansmultipro.co.id/api/recruitment/positions/${id}`
-        console.log(url);
         axios.get(url)
             .then(raw => {
                 res.send(raw.data);
