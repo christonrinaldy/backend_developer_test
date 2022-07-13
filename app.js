3921e92880f1095df371593fe35c0e3c938f9c65
christonrinaldy <christonrinaldy.geodesy@gmail.com>
2022-07-13 21:11:11 +0700

    add server app js

---
+++ app.js
@@ -0,0 +1,14 @@
+require("dotenv").config()
+const express = require("express")
+const app = express();
+const port = process.env.PORT || 3001
+// const router = require('./router/index');
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+
+// app.use(router);
+app.listen(port, () => {
+    console.log("app is listening", port)
+})
