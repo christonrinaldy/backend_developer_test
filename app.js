db8e6d3ae7f30089b41f566be1dc33320ff6768c
christonrinaldy <christonrinaldy.geodesy@gmail.com>
2022-07-13 21:25:02 +0700

    edit app.js

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ require("dotenv").config()
 const express = require("express")
 const app = express();
 const port = process.env.PORT || 3001
-// const router = require('./router/index');
+const router = require('./router/index');
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 
-// app.use(router);
+app.use(router);
 app.listen(port, () => {
     console.log("app is listening", port)
 })
