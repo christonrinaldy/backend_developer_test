b099b711496621d2933f1d8a710618bf06e577d9
christonrinaldy <christonrinaldy.geodesy@gmail.com>
2022-07-13 20:46:39 +0700

    add config for sequelize

---
+++ config/config.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+require('dotenv').config();
+module.exports = {
+  "development": {
+    "username": String(process.env.DB_USERNAME_DEV),
+    "password": String(process.env.DB_PW_DEV),
+    "database": "database_development",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": String(process.env.DB_USERNAME_TEST),
+    "password": String(process.env.DB_PW_TEST),
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": String(process.env.DB_USERNAME_PROD),
+    "password": String(process.env.DB_USERNAME_PW_PROD),
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  }
+}
